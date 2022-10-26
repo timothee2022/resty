a69ff0d4ec2a21dc216a0974319e2b2051685e95
Timothee Odushina <hsp20tim@gmail.com>
2022-10-26 04:27:17 +0000

    added index

---
+++ src/components/header/header.test.js
@@ -0,0 +1,17 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+
+import Header from'./index.js';
+
+describe('The Header Component', () => {
+  it('renders h1', () => {
+    render(<Header greeting="Test"/>);
+
+    let h1 = screen.getByTestId('header-h1');
+    expect(h1).toHaveTextContent('RESTy');
+
+    let anotherH1Example = screen.getByText('RESTy');
+    expect(anotherH1Example).toBeTruthy();
+    expect(anotherH1Example).toBeInTheDocument();
+  });
+});
